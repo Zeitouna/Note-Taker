3430d927f26618afbd701d3808cb0501c8cd18b8
Olivier R.Measson <olivierberlin1998@hotmail.de>
2022-02-06 16:54:26 +0100

    Incorate post request to create a note with id

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
-const functions = require("./helpers");
+const { v4 } = require("uuid");
+
+const { readAndAppend, readFromFile } = require("./helpers");
 
 const app = express();
 
@@ -9,12 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/notes", (req, res) => {
-  const db = functions.readFromFile("./db/db.json");
+  const db = readFromFile("./db/db.json");
   res.json(db);
 });
 
 app.post("/api/notes", (req, res) => {
-  functions.readAndAppend(req.body, "./db/db.json");
+  const data = { title: req.body.title, text: req.body.text, id: v4() };
+  readAndAppend(data, "./db/db.json");
   res.json({ msg: "success" });
 });
 
